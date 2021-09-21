7b33c36af73707bcc0843376ccf618de4c30ecfb
Juan Cleber <juan.fsj@gmail.com>
2021-09-20 23:45:25 -0300

    primeiro commit

---
+++ teste01.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+ls /root/
+
+ls /home/
+
+ls /tmp/teste
+
+RETURN_CODE=15000
+
+echo "O codigo de retorno do ls foi $RETURN_CODE"
---
+++ teste02.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+####################################################
+#	Script que busca nomes no diretorio.	   #
+#	Autor: Juan Cleber Faria Alves		   #
+#	Descriacaoo: Estudos do curso Udemy.	   #
+#						   #
+#	Data 20/09/2021.			   #
+#						   #
+####################################################
+
+ARQNOMES="/home/teste/nomes_teste.txt"
+
+clear
+echo "===== Script de Busca de Alunos ====="
+
+
