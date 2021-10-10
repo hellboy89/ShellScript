966eed2b6032cb6c23c332c507a05241ed03d9d4
root <root@kali>
2021-10-09 23:36:52 -0300

    exercicios logs email

--- exercicios/exerEmail02.sh
+++ exercicios/exerEmail02.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
+clear
+
+fileLog="exerEmail02_log.log"
+
+echo -e "\n=====> Verificando Porcentagem Utilizada do Disco $(date +"%d/%m/%y - %H:%M:%S") <=====\n" >> $fileLog
+
+while [ true ]
+do
+	porcUsada=$(df -h /dev/sda1 | awk '{print $5}' | tail -n 1 | sed 's/%//')
+	dataVerifica=$(date +"%d/%m/%y - %H:%M:%S")
+
+	if [ $porcUsada -ge 80 ]
+	then
+		# Se tiver variável no echo abaixo ele não envia e-mail.
+
+		echo "disco chegou a mais de 80%" | ssmtp juan.fsj@gmail.com
+		echo "$date, disco chegou a mais de 80%" >> $fileLog
+		echo "e-mail enviado" >> $fileLog
+		break
+	else
+		echo "$dataVerifica, ainda está no limite aceitável, em $porcUsada%" >> $fileLog
+	fi
+
+	sleep 3
+
+done
+
+
+<<anotacoes
+
+Comando abaixo pega a porcentagem total utilizada do disco.
+
+# df -h /dev/sda1 | awk '{print $5}' | tail -n 1 | sed 's/%//'
+
+anotacoes
 
---
+++ exercicios/exerEmail03.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+
+
+
+<<anotacoes
+
+Linha de baixo irá pegar sometne o que está sendo utilizado no swap.
+
+# free -m | awk '{print $3}' | tail -n 1
+
+
+anotacoes
