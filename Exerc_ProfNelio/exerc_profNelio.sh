1e4d9e116ad32df9a941361c496ee5b7098deba3
test sec <teste@sectest>
2022-07-10 22:13:26 -0300

    fazendo exercicios nelio2

--- Exerc_ProfNelio/exerc_profNelio.sh
+++ Exerc_ProfNelio/exerc_profNelio.sh
@@ -270,7 +270,467 @@ function trocoVerificado_04() {
 }
 
 function glicose_04() {
-    # PAREI AQUI..
+    read -p "Digite a medida da glicose: " mediaGlic
+
+    if [[ $mediaGlic < 100 ]]
+    then
+        echo "classificacao = normal"
+    elif [[ $mediaGlic -gt 100 ]] && [[ $mediaGlic -le 140 ]]
+    then
+        echo "classificacao = elevado"
+    else
+        echo "classificacao = diabetes"
+    fi
+
 }
 
-glicose_04
+function dardo_04() {
+    echo "Digite as tres distancias: "
+    read -p "" dist1
+    read -p "" dist2
+    read -p "" dist3
+
+    maior=$dist1
+
+    if [[ $maior -lt $dist2 ]]
+    then
+        maior=$dist2
+    fi
+    
+    if [[ $maior -lt $dist3 ]]
+    then
+        maior=$dist3
+    fi
+    
+    echo "Maior Distancia = $maior"
+
+}
+
+function temperatura_04() {
+    read -p "vc vai digitar a temperatura em qual escala (c/f): " escEscala
+
+    if [[ $escEscala = "f" ]]
+    then
+        read -p "Digite a temperatura em Fahrenheit: " tempFar
+        calc=$(echo "scale=2; (($tempFar - 32) / 1.8)" | bc)
+        echo "Temperatura equivalente em Celsius: $calc"
+    else
+        read -p "Digite a temperatura em Celcius: " tempCel
+        calc=$(echo "scale=2; ($tempCel * 1.8) + 32" | bc)
+        echo "Temperatura equivalente em Farenheit: $calc"
+    fi    
+}
+
+function lanchonete_04() {
+    read -p "Codigo do produto comprado: " codProd
+    read -p "Quantidade comprada: " quantComp
+
+    if [[ $codProd -eq 1 ]]
+    then
+        calc=$(echo "scale=2; $quantComp * 5.00" | bc)
+        echo "valor a pagar: R$ $calc"
+    elif [[ $codProd -eq 2 ]]
+    then
+        calc=$(echo "scale=2; $quantComp * 3.50" | bc)
+        echo "valor a pagar: R$ $calc"
+    elif [[ $codProd -eq 3 ]]
+    then
+        calc=$(echo "scale=2; $quantComp * 4.80" | bc)
+        echo "valor a pagar: R$ $calc"
+    elif [[ $codProd -eq 4 ]]
+    then
+        calc=$(echo "scale=2; $quantComp * 8.90" | bc)
+        echo "valor a pagar: R$ $calc"
+    elif [[ $codProd -eq 5 ]]
+    then
+        calc=$(echo "scale=2; $quantComp * 7.32" | bc)
+        echo "valor a pagar: R$ $calc"
+    else
+        echo "valor incorreto"
+    fi
+}
+
+function multiplos_04() {
+    echo "Digite dois valores inteiros: "
+    read -p "" num1
+    read -p "" num2
+
+    if [[ $(expr $num1 % $num2) = 0 ]] || [[ $(expr $num2 % $num1) = 0 ]]
+    then
+        echo "sao MULTIPLOS"
+    else
+        echo "NAO sao MULTIPLOS"
+    fi
+}
+
+function aumento_04() {
+    read -p "Digite o salario da pessoa: " salario
+
+    if [[ $salario -le 1000 ]]
+    then
+        calcNovoSal=$(echo "scale=2; ($salario * 0.20) + $salario" | bc)
+        calcAumento=$(echo "scale=2; ($salario * 0.20)" | bc)
+
+        echo "novo salario = $calcNovoSal"
+        echo "aumento = $calcAumento"
+        echo "porcentatem = 20%"
+    elif [[ $salario -gt 1000 ]] && [[ $salario -le 3000 ]]
+    then
+        calcNovoSal=$(echo "scale=2; ($salario * 0.15) + $salario" | bc)
+        calcAumento=$(echo "scale=2; ($salario * 0.15)" | bc)
+
+        echo "novo salario = $calcNovoSal"
+        echo "aumento = $calcAumento"
+        echo "porcentatem = 15%"
+    elif [[ $salario -gt 3000 ]] && [[ $salario -le 8000 ]]
+    then
+        calcNovoSal=$(echo "scale=2; ($salario * 0.10) + $salario" | bc)
+        calcAumento=$(echo "scale=2; ($salario * 0.10)" | bc)
+
+        echo "novo salario = $calcNovoSal"
+        echo "aumento = $calcAumento"
+        echo "porcentatem = 10%"
+    elif [[ $salario -gt 8000 ]]
+    then
+        calcNovoSal=$(echo "scale=2; ($salario * 0.05) + $salario" | bc)
+        calcAumento=$(echo "scale=2; ($salario * 0.05)" | bc)
+
+        echo "novo salario = $calcNovoSal"
+        echo "aumento = $calcAumento"
+        echo "porcentatem = 5%"
+    else
+        echo "valor incorreto"
+    fi
+    
+}
+
+function tempoDeJogo_04() {
+    read -p "Hora Inicial: " horaInicial
+    read -p "Hora Final: " horaFinal
+
+    if [[ $horaInicial -lt $horaFinal ]]
+    then
+        calc=$(echo "$horaFinal - $horaInicial" | bc)
+    else
+        calc=$(echo "24 - ($horaInicial - $horaFinal)" | bc)
+    fi
+
+    echo "O jogo durou $calc horas."
+
+}
+
+function coordenadas_04() {
+    read -p "valor de X: " valX
+    read -p "valor de Y: " valY
+
+    if [[ $valX -gt 0 ]] && [[ $valY -lt 0 ]]
+    then
+        echo "Q4"
+    elif [[ $valX -gt 0 ]] && [[ $valY -gt 0 ]]
+    then
+        echo "Q1"
+    elif [[ $valX -eq 0 ]] && [[ $valY -eq 0 ]]
+    then
+        echo "Origem"
+    elif [[ $valX -gt 0 ]] && [[ $valY -eq 0 ]]
+    then
+        echo "Eixo X"
+    else
+        echo "valor incorreto"
+    fi
+}
+
+function crescente_04() {
+
+    while :; do
+        echo "Digite dois numeros: "
+        read -p "" val1
+        read -p "" val2
+
+        if [[ $val1 -gt $val2 ]]
+        then
+            echo "crescente"
+        elif [[ $val1 -eq $val2 ]]
+        then
+            break
+        else
+            echo "decrescente"
+        fi
+
+    done
+
+}
+
+function testesWhile() {
+    read -p "digite um valor para teste: " valTeste
+
+    while :; do
+        read -p "digite 0 para parar: " val
+
+        if [[ $val -eq 0 ]]
+        then
+            echo "PARADO!"
+            break
+        fi
+
+        echo "valor digitado: $val"
+    done
+}
+
+function mediaIdades_04() {
+    echo "digite as idades: "
+    soma=0
+    contador=0
+
+    while :
+    do
+        read -p "" val
+
+        if [[ $contador -eq 0 ]] && [[ $val -lt 0 ]]
+        then
+            echo "IMPOSSIVEL CALCULAR"
+            break
+        fi
+
+        if [[ $val -gt 0 ]]
+        then
+            soma=$(echo "$val + $soma" | bc)
+            contador=$(echo "$contador + 1" | bc)
+        else
+            media=$(echo "scale=2; $soma / $contador" | bc)
+            echo "Media = $media"
+            break
+        fi
+
+    done
+}
+
+function senhaFixa_04() {
+    read -p "Digite a senha: " senha
+
+    while :;
+    do
+
+        if [[ $senha -eq 2002 ]]
+        then
+            echo "Acesso Permitido!"
+            break
+        else
+            read -p "senha invalida, tente novamente: " senha
+        fi
+    done
+
+}
+
+function quadrante_04() {
+    while :
+    do
+        echo "Digite os valores das coordenadas X e Y: "
+
+        read -p "" valX
+        read -p "" valY
+
+        if [[ $valX -gt 0 ]] && [[ $valY -gt 0 ]]
+        then
+            echo "Quadrante Q1"
+        elif [[ $valX -gt 0 ]] && [[ $valY -lt 0 ]]
+        then
+            echo "Quadrante Q4"
+        elif [[ $valX -lt 0 ]] && [[ $valY -gt 0 ]]
+        then
+            echo "Quadrante Q2"
+        elif [[ $valX -lt 0 ]] && [[ $valY -lt 0 ]]
+        then
+            echo "Quadrante Q3"
+        elif [[ $valX -eq 0 ]] || [[ $valY -eq 0 ]]
+        then
+            break
+        else
+            echo "valor incorreto!"
+            break
+        fi
+    done
+
+}
+
+function validacaoDeNota_04() {
+    # ============== NOTA1
+    read -p "Digite a primeira nota: " nota1
+    somaNotas=0
+
+    while :
+    do
+        if [[ $nota1 -gt 10 ]] || [[ $nota1 -lt 0 ]]
+        then
+            read -p "Valor incorreto! Tente novamente: " nota1
+        else
+            somaNotas=$(echo "$somaNotas + $nota1" | bc)
+            break
+        fi
+    done
+
+    # ============== NOTA2
+    read -p "Digite a segunda nota: " nota2
+
+    while :
+    do
+        if [[ $nota2 -gt 10 ]] || [[ $nota2 -lt 0 ]]
+        then
+            read -p "Valor incorreto! Tente novamente: " nota2
+        else
+            somaNotas=$(echo "$somaNotas + $nota2" | bc)
+            break
+        fi
+    done
+
+    calc=$(echo "scale=2; $somaNotas / 2" | bc)
+
+    echo "Media = $calc"
+
+}
+
+function testeContadores() {
+    contador=0
+    soma=0
+
+    while :
+    do
+        read -p "digite um valor para a soma: " val
+
+        soma=$(echo "$soma + $val" | bc)
+
+        echo "a soma esta em: $soma"
+
+        if [[ $val -eq 0 ]]
+        then
+            break
+        fi
+    done
+
+    while :
+    do
+        read -p "digite um segundo valor para a soma: " val2
+
+        soma=$(echo "$soma + $val2" | bc)
+
+        echo "a soma esta em: $soma"
+
+        if [[ $val2 -eq 0 ]]
+        then
+            break
+        fi
+    done
+}
+
+function combustivel_04() {
+    alcool=0
+    gasolina=0
+    diesel=0
+
+    while :
+    do
+        read -p "informe um codigo (1, 2, 3) ou 4 para parar: " valor
+
+
+        if [[ $valor -eq 1 ]]
+        then
+            alcool=$(echo "$alcool + 1" | bc)
+        elif [[ $valor -eq 2 ]]
+        then
+            gasolina=$(echo "$gasolina + 1" | bc)
+        elif [[ $valor -eq 3 ]]
+        then
+            diesel=$(echo "$diesel + 1" | bc)
+        elif [[ $valor -eq 4 ]]
+        then
+            break
+        else
+            $teste
+        fi
+    done
+
+    echo "Muito Obrigado"
+    echo "Alcool: $alcool"
+    echo "Gasolina: $gasolina"
+    echo "Diesel: $diesel"
+}
+
+function paresConsecutivos_04() {
+    read -p "digite um numero inteiro: " num
+
+    while [ $num -ne 0 ]
+    do
+        if [[ $(expr $num % 2) != 0 ]]
+        then
+            num=$(echo "$num + 1" | bc)
+        fi
+
+        soma=$(echo "(5 * $num) + 20" | bc)
+
+        echo "soma = $soma"
+
+        read -p "digite um numero inteiro: " num
+
+    done
+}
+
+function testeFor1() {
+    for i in {1..10}
+    do
+        echo "contador: $i"
+    done
+}
+
+function tabuada_04() {
+    read -p "deseja a tabuada de qual valor: " num
+
+    for i in {1..10}
+    do
+        calc=$(echo "$num * $i" | bc)
+        echo "$num x $i = $calc"
+    done
+}
+
+function somaImpares_04() {
+    echo "digite dois numeros: "
+    read -p "" val1
+    read -p "" val2
+
+    if [[ $va1 -gt $val2 ]]
+    then
+        troca=$val1
+        val1=$val2
+        val2=$troca
+    fi
+
+    soma=0
+
+    # PAREI AQUI, TENHO QUE SABER COMO FAZ UM FOR, SOMANDO VALORES
+    # COMO ESTÁ NA CORREÇÃO.
+
+    calc=$(echo "$val1 + 1" | bc)
+    for i in $(seq $calc $val2)
+    do
+        if [[ $(expr $i % 2) != 0 ]]
+        then
+            soma=$(echo "$soma + $i" | bc)
+        fi
+    done
+
+    echo "soma dos impares = $soma"
+
+}
+
+function testeFor2() {
+
+    val=$(echo "1 + 1" | bc)
+    
+    for i in $(seq $val 20)
+    do
+        echo "contador $i"
+    done
+
+}
+
+# testeFor2
+somaImpares_04
